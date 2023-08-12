b34d020f1e85790d2a16c48d73bea76b44defc21
Peter Navarrete <navarrep1@newpaltz.edu>
2023-08-12 18:16:09 -0400

    tmux pane switching

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -57,35 +57,53 @@ local plugins = {
         end,
     },
     {
-    "alexghergh/nvim-tmux-navigation", config = function()
-        local nvim_tmux_nav = require('nvim-tmux-navigation')
-        nvim_tmux_nav.setup {
-            disable_when_zoomed = true -- defaults to false
-        }
-        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-        vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-        vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+        "alexghergh/nvim-tmux-navigation",
+        lazy = false,
+        config = function()
+            local nvim_tmux_nav = require('nvim-tmux-navigation')
+            nvim_tmux_nav.setup {
+                disable_when_zoomed = true -- defaults to false
+            }
+            vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+            vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+            vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+            vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+            vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+            vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
 
-    end
-    }
-
-
--- To make a plugin not be loaded
--- {
---   "NvChad/nvim-colorizer.lua",
---   enabled = false
--- },
-
--- All NvChad plugins are lazy-loaded by default
--- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
--- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
--- {
---   "mg979/vim-visual-multi",
---   lazy = false,
--- }
+        end
+    },
+    {
+        "alexghergh/nvim-tmux-navigation", config = function()
+            require'nvim-tmux-navigation'.setup {
+                disable_when_zoomed = true, -- defaults to false
+                keybindings = {
+                    left = "<C-h>",
+                    down = "<C-j>",
+                    up = "<C-k>",
+                    right = "<C-l>",
+                    last_active = "<C-\\>",
+                    next = "<C-Space>",
+                }
+            }
+        end
+    },
 }
+
+
+    -- To make a plugin not be loaded
+    -- {
+    --   "NvChad/nvim-colorizer.lua",
+    --   enabled = false
+    -- },
+
+    -- All NvChad plugins are lazy-loaded by default
+    -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+    -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+    -- {
+    --   "mg979/vim-visual-multi",
+    --   lazy = false,
+    -- }
+
 
 return plugins
