b4fa4da6d0838765e0b5830360d5643ec67df0cd
Peter Navarrete <navarrep1@newpaltz.edu>
2023-08-13 08:25:08 -0400

    some updates to init

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -8,3 +8,29 @@
 
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
+
+
+-- local bufnr = 1
+--
+--
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+    -- group = vim.api.nvim_create_augroup("TjsCoolTutorial", { clear = true }),
+--     pattern = "main.go",
+--
+--     callback = function()
+--         -- vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {"outpuut of main.go"})
+--         vim.fn.jobstart({"go", "run", "."}, {
+--             stdout_buffered = true,
+--             on_stdout = function(_, data)
+--             if data then
+--                 -- vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+--             end
+--         end,
+--             on_stderr = function(_, data)
+--             if data then
+--                 -- vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+--             end
+--         end,
+--         })
+--     end,
+-- })
